0d153ca4b05111eb776c7be03b042c133c492c39
Aylin Bircan <aylinsonlu@gmail.com>
2022-04-25 22:49:48 +0300

    Corrected the path

--- download_gtf.py
+++ download_gtf.py
@@ -15,10 +15,10 @@ for species_directory in species_directories:
     host.chdir('/pub/release-106/gtf/' +species_directory + '/')
     gtf_files = host.listdir(host.curdir)
     for gtf_file in gtf_files:
-        print(gtf_file)
+        #print(gtf_file)
         if "106.gtf.gz"  in gtf_file:
             os.chdir(base_path)
             #os.mkdir(os.path.join(base_path))
             host.keep_alive()
-            host.download('/pub/release-106/gtf/' +species_directory +gtf_file, os.path.join(base_path,gtf_file))
+            host.download('/pub/release-106/gtf/' +species_directory+ '/' +gtf_file, os.path.join(base_path,gtf_file))
      
