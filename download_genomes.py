8e89f3a11447af304eb8e8b8b47f573be2242960
Aylin Bircan <aylinsonlu@gmail.com>
2022-06-20 20:14:05 +0300

    Changed paths

--- download_genomes.py
+++ download_genomes.py
@@ -5,29 +5,40 @@ import ftputil
 import shutil
 
 
-base_path = "/cta/users/abircan/Genomes_release_106/"
-host = ftputil.FTPHost('ftp.ensembl.org','anonymous','password')
 
-host.chdir('/pub/release-106/fasta/')
-genomes_directories = host.listdir(host.curdir)
 
-for genomes_directory in genomes_directories:
-    host.chdir('/pub/release-106/fasta/' +genomes_directory + '/')
-    sub_genomes_directories = host.listdir(host.curdir)
-    for sub_genomes_directory in sub_genomes_directories:
-        if sub_genomes_directory == "dna":
-            host.chdir('/pub/release-106/fasta/' +genomes_directory + '/dna/')
-            dna_files = host.listdir(host.curdir)
-            for dna_file in dna_files:
-                if ".dna_sm.primary_assembly.fa.gz" in dna_file:
-                    os.chdir(base_path)
-                    #os.mkdir(os.path.join(base_path))
-                    host.keep_alive()
-                    host.download('/pub/release-106/fasta/' +genomes_directory + '/dna/'+dna_file, os.path.join(base_path,dna_file))
-                elif  "dna_sm.toplevel.fa.gz" in dna_file:
-                    os.chdir(base_path)
-                    #os.mkdir(os.path.join(base_path))
-                    host.keep_alive()
-                    host.download('/pub/release-106/fasta/' +genomes_directory + '/dna/'+dna_file, os.path.join(base_path,dna_file))
-               
+def download_genomes(base_path):
+    host = ftputil.FTPHost('ftp.ensembl.org','anonymous','password')
+
+    host.chdir('/pub/release-106/fasta/')
+    genomes_directories = host.listdir(host.curdir)
+
+    for genomes_directory in genomes_directories:
+        host.chdir('/pub/release-106/fasta/' +genomes_directory + '/')
+        sub_genomes_directories = host.listdir(host.curdir)
+        for sub_genomes_directory in sub_genomes_directories:
+            if sub_genomes_directory == "dna":
+                host.chdir('/pub/release-106/fasta/' +genomes_directory + '/dna/')
+                dna_files = host.listdir(host.curdir)
+                for dna_file in dna_files:
+                    if ".dna_sm.primary_assembly.fa.gz" in dna_file:
+                        species_name = dna_file.split(".")[0]
+                        os.makedirs(base_path+species_name)
+                        download_path = base_path+species_name
+                        os.chdir(download_path)
+                        #os.mkdir(os.path.join(base_path))
+                        host.keep_alive()
+                        host.download('/pub/release-106/fasta/' +genomes_directory + '/dna/'+dna_file, os.path.join(download_path,species_name+".fasta"))
+                    elif  "dna_sm.toplevel.fa.gz" in dna_file:
+                        species_name = dna_file.split(".")[0]
+                        os.makedirs(base_path+species_name)
+                        download_path = base_path+species_name
+                        os.chdir(download_path)
+                        #os.mkdir(os.path.join(base_path))
+                        host.keep_alive()
+                        host.download('/pub/release-106/fasta/' +genomes_directory + '/dna/'+dna_file, os.path.join(download_path,species_name+".fasta"))
+                
+if __name__ == "__main__":
+    base_path = sys.argv[1]
+    download_genomes(base_path)
 
--- download_gtf.py
+++ download_gtf.py
@@ -4,21 +4,28 @@ import sys
 import ftputil
 import shutil
 
-#base_path = "/Users/aylin/Desktop/"
-base_path = "/cta/users/abircan/GTFs_release_106/"
-host = ftputil.FTPHost('ftp.ensembl.org','anonymous','password')
+def download_gtf(base_path):
 
-host.chdir('/pub/release-106/gtf/')
-species_directories = host.listdir(host.curdir)
+    host = ftputil.FTPHost('ftp.ensembl.org','anonymous','password')
 
-for species_directory in species_directories:
-    host.chdir('/pub/release-106/gtf/' +species_directory + '/')
-    gtf_files = host.listdir(host.curdir)
-    for gtf_file in gtf_files:
-        #print(gtf_file)
-        if "106.gtf.gz"  in gtf_file:
-            os.chdir(base_path)
-            #os.mkdir(os.path.join(base_path))
-            host.keep_alive()
-            host.download('/pub/release-106/gtf/' +species_directory+ '/' +gtf_file, os.path.join(base_path,gtf_file))
-     
+    host.chdir('/pub/release-106/gtf/')
+    species_directories = host.listdir(host.curdir)
+
+    for species_directory in species_directories:
+        host.chdir('/pub/release-106/gtf/' +species_directory + '/')
+        gtf_files = host.listdir(host.curdir)
+        for gtf_file in gtf_files:
+            #print(gtf_file)
+            if "106.gtf.gz"  in gtf_file:
+                species_name = gtf_file.split(".")[0]
+                os.makedirs(base_path+species_name)
+                download_path = base_path+species_name
+                os.chdir(download_path)
+                #os.mkdir(os.path.join(base_path))
+                host.keep_alive()
+                host.download('/pub/release-106/gtf/' +species_directory+ '/' +gtf_file, os.path.join(download_path,species_name+".gtf"))
+    
+
+if __name__ == "__main__":
+    base_path = sys.argv[1]
+    download_gtf(base_path)
